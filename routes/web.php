a05ec790361d5cae43c8cc247193490302f1a247
dorothydiaz <dorothydiaz@outlook.ph>
2024-04-12 02:38:03 +0800

    1st commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pages\HomePage;
+use App\Http\Controllers\language\LanguageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+//Redirect to login page
+Route::redirect(uri: '/', destination: 'login');
+
+// locale
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
+
+Route::middleware([
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
+])->group(function () {
+  Route::get('/dashboard', function () {
+    return view('dashboard');
+  })->name('dashboard');
+
+  // Main Page Route
+  Route::get('/', [HomePage::class, 'index'])->name('pages-home');
+});
